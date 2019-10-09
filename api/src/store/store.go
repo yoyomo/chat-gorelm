98943783328c919f5e3a73585953acdf9a5bf961
Armando Ortiz <armando@solairo.co.jp>
2019-10-09 17:49:38 +0900

    working go environment

--- api/src/store/store.go
+++ api/src/store/store.go
@@ -13,52 +13,6 @@ import (
 
 var DB *sql.DB
 
-// (2)投稿1件の取得
-func GetPost(id int) (post db.Post, err error) {
-	post = Post{}
-	err = DB.QueryRow("select id,content,author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
-	return
-}
-
-// (3)新規投稿の生成
-func (post *Post) Create() (err error) {
-	statement := "insert into posts (content,author) values ($1,$2) returning id"
-	stmt, err := DB.Prepare(statement)
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
-	return
-}
-
-func (post *Post) Update() (err error) {
-	_, err = DB.Exec("update posts set content = $2,author=$3 where id = $1", post.Id, post.Content, post.Author)
-	return
-}
-
-func (post *Post) Delete() (err error) {
-	_, err = DB.Exec("delete from posts where id=$1", post.Id)
-	return
-}
-
-func Posts(limit int) (posts []Post, err error) {
-	rows, err := DB.Query("select id, content,author from posts limit $1", limit)
-	if err != nil {
-		return
-	}
-	for rows.Next() {
-		post := Post{}
-		err = rows.Scan(&post.Id, &post.Content, &post.Author)
-		if err != nil {
-			return
-		}
-		posts = append(posts, post)
-	}
-	rows.Close()
-	return
-}
-
 func connectToDB() {
 
 	connStr := "user=pqgotest dbname=pqgotest sslmode=disable"
@@ -79,7 +33,7 @@ func main() {
 
 	connectToDB()
 
-	post := Post{
+	post := db.Post{
 		Content: "Hello",
 		Author:  "Taro",
 	}
@@ -89,7 +43,7 @@ func main() {
 
 	fmt.Println(post)
 
-	readPost, _ := GetPost(post.Id)
+	readPost, _ := db.GetPost(post.Id)
 	readPost.Author = "Poerre"
 	readPost.Update()
 
